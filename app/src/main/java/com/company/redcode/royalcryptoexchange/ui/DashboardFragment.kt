4161ebf9c4f68e37e9c4658fd8ef7e639ff5ced9
Zulqarnain <zuluparvi@gmail.com>
2018-09-10 17:31:05 +0500

    logo integration

---
+++ app/src/main/java/com/company/redcode/royalcryptoexchange/ui/DashboardFragment.kt
@@ -0,0 +1,21 @@
+package com.company.redcode.royalcryptoexchange.ui
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.company.redcode.royalcryptoexchange.R
+
+
+class DashboardFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+
+}
